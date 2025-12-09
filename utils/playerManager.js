507a57d2e983e586548d3665bb41612c6f6838ae
I-L9614 <leviitamar034@gmail.com>
2025-12-09 15:58:38 +0200

    added addPlayer

---
+++ utils/playerManager.js
@@ -0,0 +1,49 @@
+import { players } from "../data/players.js";
+
+export function getAllPlayers() {
+    return players
+}
+
+
+export const findPlayerById = (id) => {
+    return players.find(player => player.id === id)
+}
+
+// function createPlayer(name,position,age,nationality,goals=0,assists=0,matches=0) {
+//     return {
+//         id:players.length+1,
+//         name,
+//         position,
+//         age,
+//         goals,
+//         assists,
+//         matches,
+//         nationality
+//     }
+// }
+
+
+// function addPlayer(playerData) {
+//     const player = createPlayer(playerData.name,playerData.position,playerData.age,playerData.nationality,playerData.goals,playerData.assists,playerData.matches)
+//     player.id = players.length+1
+//     players.push(player)
+//     return player
+// }
+
+
+function addPlayer(playerData) {
+    const newPlayer = {
+        id: players.length + 1,
+        name: playerData.name,
+        position: playerData.position,
+        age: playerData.age,
+        goals: 0,
+        assists: 0,
+        matches: 0,
+        nationality: playerData.nationality
+    }
+    players.push(newPlayer)
+    return newPlayer
+}
+addPlayer({name:"itamar",position:"Forward",age:21,nationality:"Israeli"})
+console.log(getAllPlayers())
