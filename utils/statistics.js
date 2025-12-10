a8029ffd4e1b74e04f4c7e77d1c65804fbfc72c0
I-L9614 <leviitamar034@gmail.com>
2025-12-10 11:17:58 +0200

    added get top scorer function & get average age function

--- utils/statistics.js
+++ utils/statistics.js
@@ -7,3 +7,18 @@ export function getTotalGoals() {
 export function getPlayerByPosition(position) {
     return players.filter(player => player.position===position)
 }
+
+export function getTotalAssist() {
+    return players.reduce((total, player) => total + player.assists, 0);
+}
+
+export function getTopscorer() {
+    const getMax = (a, b) => Math.max(a, b)
+    return players.reduce((accumulator, currentValue) => accumulator = getMax(accumulator,currentValue.goals),0 );
+}
+
+export function getAverageAge() {
+    const totalAge = players.reduce((total, player) => total + player.age, 0)
+    return totalAge/players.length
+}
+
