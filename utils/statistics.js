d40ed7e3d49d69e61eaab9881b5a63480e87f770
I-L9614 <leviitamar034@gmail.com>
2025-12-10 10:07:44 +0200

    add get player by position

--- utils/statistics.js
+++ utils/statistics.js
@@ -3,4 +3,7 @@ import { players } from "../data/players.js";
 export function getTotalGoals() {
     return players.reduce((total, player) => total + player.goals, 0);
 }
-console.log(getTotalGoals())
+
+export function getPlayerByPosition(position) {
+    return players.filter(player => player.position===position)
+}
